fea6339936ee43f84a8c0c092ccbd8248b4f0f62
MISTOM <kigardetom2001@gmail.com>
2022-06-14 14:08:23 +0300

    eslint

--- utils/redis.js
+++ utils/redis.js
@@ -1,20 +1,20 @@
-import { createClient } from 'redis'
-import { promisify } from 'node:util'
+import { createClient } from 'redis';
+import { promisify } from 'node:util';
 /**
  * RedisClient: Represents a redis client
  */
-class RedisClient{
+class RedisClient {
   /**
    * creates a new RedisClient instance
    */
-  constructor(){
-    this.client = createClient()
-    this.clientIsConnected = true
-    this.client.on('error', err => {
-      console.error('Redis client failed to connnect: ', err.message || err.toString())
-      this.clientIsConnected = false
-    })
-    this.client.on('connect', () => this.clientIsConnected = true) 
+  constructor() {
+    this.client = createClient();
+    this.clientIsConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connnect: ', err.message || err.toString());
+      this.clientIsConnected = false;
+    });
+    this.client.on('connect', () => { this.clientIsConnected = true; });
   }
 
   /**
@@ -22,21 +22,21 @@ class RedisClient{
    *  otherwise, false
    * @returns {boolean}
    */
-  isAlive(){
-    return this.clientIsConnected
+  isAlive() {
+    return this.clientIsConnected;
   }
 
-  async get(key){
-    return promisify(this.client.GET).bind(this.client)(key)
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
   }
 
-  async set(key, value, duration){
+  async set(key, value, duration) {
     await promisify(this.client.SETEX)
-      .bind(this.client)(key, value, duration)
+      .bind(this.client)(key, value, duration);
   }
 
-  async del(key){
-    await promisify(this.client.DEL).bind(this.client)(key)
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
   }
 }
 
