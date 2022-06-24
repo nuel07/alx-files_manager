30ce7cd5592f6108d58d67136b12d4a9afa93479
nuel07 <yehanganep@gmail.com>
2022-06-24 01:03:28 -0700

    redone

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,39 +6,39 @@ import dbClient from '../utils/db';
 const userQ = new Queue('userQ');
 
 class UsersController {
-    static async postNew(req, res) {
-	const { email, password } = req.body;
-	if (!email) return res.status(400).send({ error: 'Missing email' });
-	if (!password) return res.status(400).send({ error: 'Missing password' });
-	const emailExists = await dbClient.users.findOne({ email });
-	if (emailExists) return res.status(400).send({ error: 'Already exist' });
-	const secPass = sha1(password);
-	const insertStat = await dbClient.users.insertOne({
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+    const emailExists = await dbClient.users.findOne({ email });
+    if (emailExists) return res.status(400).send({ error: 'Already exist' });
+    const secPass = sha1(password);
+    const insertStat = await dbClient.users.insertOne({
 	    email,
-	    password: secPass, 
-	});
-	const createdUser = {
+	    password: secPass,
+    });
+    const createdUser = {
 	    id: insertStat.insertedId,
-	    email,    
-	};
-	
-	await userQ.add({
-	    userId: insertStat.insertedId.toString(), 
-	});
-	return res.status(201).send(createdUser);	
-    }
-   
-    static async getMe(req, res) {
-	const user_token = request.header('X-Token');
-	const user_key = `auth_${user_token}`;
-	const user = await dbClient.users.findOne({ _id: ObjectId(userId) });	
-	if (!user) return res.status(401).send({ error: 'Unauthorized' });
+	    email,
+    };
 
-	const userInfo = { id: user._id, ...user };
-	delete userInfo._id;
-	delete userInfo.password;
-	return res.status(200).send(userInfo);	
-    }    
+    await userQ.add({
+	    userId: insertStat.insertedId.toString(),
+    });
+    return res.status(201).send(createdUser);
+  }
+
+  static async getMe(req, res) {
+    const user_token = request.header('X-Token');
+    const user_key = `auth_${user_token}`;
+    const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    const userInfo = { id: user._id, ...user };
+    delete userInfo._id;
+    delete userInfo.password;
+    return res.status(200).send(userInfo);
+  }
 }
 
 export default UsersController;
